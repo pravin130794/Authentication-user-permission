913f5074cdc98821b7ae80477b98aaeb64926d71
pravin130794 <pravin130794@gmail.com>
2017-06-14 15:50:13 +0530

    config
    a database connection in connection.js and model.js

---
+++ config/bootstrap.js
@@ -0,0 +1,17 @@
+/**
+ * Bootstrap
+ * (sails.config.bootstrap)
+ *
+ * An asynchronous bootstrap function that runs before your Sails app gets lifted.
+ * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
+ *
+ * For more information on bootstrapping your app, check out:
+ * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
+ */
+
+module.exports.bootstrap = function(cb) {
+  sails.sha1js = require('sha1');
+  cb();
+  // It's very important to trigger this callback method when you are finished
+  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+};
